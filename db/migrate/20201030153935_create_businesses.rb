ee1659bcabbf7f6981ba7b5fdea28ccad4f18302
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-30 09:11:38 -0700

    create seed file

--- db/migrate/20201030153935_create_businesses.rb
+++ db/migrate/20201030153935_create_businesses.rb
@@ -1,7 +1,10 @@
 class CreateBusinesses < ActiveRecord::Migration[5.2]
   def change
     create_table :businesses do |t|
-
+      t.string :name
+      t.string :zipcode
+      t.string :address
+      t.string :phone
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_businesses
+  end
+
+  def generate_businesses
+    20.times do |i|
+      business = Bussiness.create(
+        name: 
+        zipcode:
+        address:
+        phone:
+      )
+      puts "Business #{i}: #{business.name} is located in the #{business.zipcode}."
+    end
+  end
+end
+
+Seed.begin
