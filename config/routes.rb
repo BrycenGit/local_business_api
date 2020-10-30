e5cd789cba496e0e12021715a01fc721e887774d
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-30 10:14:00 -0700

    pass get businesses spec

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # get 'businesses/update'
   # get 'businesses/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :bussinesses
+resources :businesses
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory(:business) do
+    name {Faker::Restaurant.name}
+    address {Faker::Address.street_address}
+    phone {Faker::PhoneNumber.phone_number}
+    zipcode { '97006' }
+  end
+end
