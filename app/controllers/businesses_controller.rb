1e4be1c62463a4eada4a78064649d23f843d131d
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-30 09:30:27 -0700

    add controller methods to business controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,16 +1,31 @@
 class BusinessesController < ApplicationController
   def index
+    @businesses = Business.all
+
   end
 
   def show
+    @business = Business.find(params[:id])
+
   end
 
   def create
+    @business = Business.create(business_params)
+
   end
 
   def update
+    @business = Business.find(params[:id])
+    @business.update(business_params)
   end
 
   def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+
+  private
+  def business_params
+    params.permit(:name, :zipcode, :address, :phone)
   end
 end
