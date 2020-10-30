60d6f7b8653d65dda39076cdb4f633fc83353eac
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-30 14:26:03 -0700

    trying to get token to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_161421) do
+ActiveRecord::Schema.define(version: 2020_10_30_211903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "access_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "businesses", force: :cascade do |t|
     t.datetime "created_at", null: false
