01df6a2cb49649995f9ed0bb5a20b7b943a3cacd
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-30 09:54:47 -0700

    add routes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,22 +1,24 @@
 class BusinessesController < ApplicationController
   def index
     @businesses = Business.all
-
+    json_response(@businesses)
   end
 
   def show
     @business = Business.find(params[:id])
-
+    json_response(@business)
   end
 
   def create
     @business = Business.create(business_params)
-
+    json_response(@business, :created)
   end
 
   def update
     @business = Business.find(params[:id])
-    @business.update(business_params)
+    if @business.update(business_params)
+      render status: 200, json: { message: "successful update" }
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'businesses/index'
-  get 'businesses/show'
-  get 'businesses/create'
-  get 'businesses/update'
-  get 'businesses/destroy'
+  # get 'businesses/index'
+  # get 'businesses/show'
+  # get 'businesses/create'
+  # get 'businesses/update'
+  # get 'businesses/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :bussinesses
 end
